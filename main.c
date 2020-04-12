5ad401970d4a5ab8b9667c89dcdf178b0d80d448
Tiger Nie <nhl0819@gmail.com>
2020-04-12 14:52:51 -0500

    Added dynamic string struct 'abuf' with abAppend method to extend the string. Rewrote editorScreenRefresh to append characters to abuf before printing all at once.

--- main.c
+++ main.c
@@ -3,6 +3,7 @@
 #include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sys/ioctl.h>
 #include <termio.h>
 #include <unistd.h>
@@ -151,6 +152,27 @@ int getWindowSize(int *rows, int *cols) {
     }
 }
 
+/*** dynamic string ***/
+struct abuf {
+    char *b;
+    int len;
+};
+
+#define ABUF_INIT {NULL, 0}
+
+void abAppend(struct abuf *ab, const char *s, int len) {
+    char *new = realloc(ab->b, ab->len + len);
+
+    if (new == NULL) return;
+    memcpy(&new[ab->len], s, len);
+    ab->b = new;
+    ab->len += len;
+}
+
+void abFree(struct abuf *ab) {
+    free(ab->b);
+}
+
 /*** output ***/
 
 /*
@@ -169,24 +191,36 @@ void editorClearScreen() {
 /*
  * Handle drawing each row of the text buffer being edited
  */
-void editorDrawRows() {
+void editorDrawRows(struct abuf *ab) {
     int y;
     for (y=0; y < E.screenrows; y++) {
-        write(STDOUT_FILENO, "~", 1);
+        abAppend(ab, "~", 1);
 
+        // Clear line
+        abAppend(ab, "\x1b[K", 3);
         if (y < E.screenrows -1) {
-            write(STDOUT_FILENO, "\r\n", 2);
+            abAppend(ab, "\r\n", 2);
         }
     }
 }
 
 void editorRefreshScreen() {
-    editorClearScreen();
+    struct abuf ab = ABUF_INIT;
 
-    editorDrawRows();
+    // Hide cursor
+    abAppend(&ab, "\x1b[?25l", 6);
+    // Reset cursor
+    abAppend(&ab, "\x1b[H", 3);
+
+    editorDrawRows(&ab);
 
     // Reset cursor position to 0,0
-    write(STDOUT_FILENO, "\x1b[H", 3);
+    abAppend(&ab, "\x1b[H", 3);
+    // Show cursor
+    abAppend(&ab, "\x1b[?25h", 6);
+
+    write(STDOUT_FILENO, ab.b, ab.len);
+    abFree(&ab);
 }
 
 /*** input ***/
