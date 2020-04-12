201090d04aeeaa89e3d9694110d9010336ae9cc2
Tiger Nie <nhl0819@gmail.com>
2020-04-12 13:16:28 -0500

    Enable raw mode completely and added error handling

--- main.c
+++ main.c
@@ -1,23 +1,31 @@
+/*** includes ***/
+#include <ctype.h>
+#include <errno.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <termio.h>
 #include <unistd.h>
 
+/*** data ***/
+
 struct termios orig_termios;
 
+/*** terminal ***/
+
 /*
- * Turn echo mode back on before quitting the program
+ * Errorhandling.
+ * Check each of our library calls for failure and call die when they fail
+ */
+void die(const char *s) {
+    perror(s);
+    exit(1);
+}
+
+/*
+ * Restore terminal attributes before quitting the program
  */
 void disableRawMode() {
-    struct termios raw;
-
-    // Read the current attributes into a struct
-    tcgetattr(STDIN_FILENO, &raw);
-
-    // Set the ECHO flag off
-    raw.c_lflag &= ~(ECHO);
-
-    // Write the new terminal attributes
-    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1) die("tcsetattr");
 }
 
 /*
@@ -25,27 +33,68 @@ void disableRawMode() {
  */
 void enableRawMode() {
     // Read the current attributes into a struct
-    tcgetattr(STDIN_FILENO, &orig_termios);
+    if (tcgetattr(STDIN_FILENO, &orig_termios) == -1) die("tcgetattr");
 
     // Register disableRawMode to be called at exit
     atexit(disableRawMode);
 
     // Copy the original attributes
     struct termios raw = orig_termios;
-    // Set the ECHO flag off
-    raw.c_lflag &= ~(ECHO);
+
+    // Disable input flags:
+    //     IXON: Ctrl-S and Ctrl-Q (flow control)
+    //     ICRNL: Stop translating carriage returns into newlines
+    //            Ctrl-M and Enter both read 13, '\r' instead of 10, '\n'
+    //     BRKINT: Stop break condition from sending SIGINT
+    // Other misc flags:
+    //     INPCK, ISTRIP: Flags required to enable raw mode in old
+    //          terminals that are probably already disabled
+    raw.c_iflag &= ~(BRKINT | INPCK | ISTRIP | ICRNL | IXON);
+
+    // Disable output flags:
+    //     OPOST: Stop translating newlines into carriage returns followed
+    //            by a newline ('\n' to '\r\n')
+    raw.c_oflag &= ~(OPOST);
+
+    // Set character size to 8 bits per byte. Probably already set
+    raw.c_cflag |= (CS8);
+
+    // Disable local flags:
+    //     ECHO mode (key presses aren't echoed)
+    //     Canonical mode (each key press is passed to the program instead
+    //         of new lines)
+    //     ISIG: Ctrl-C and Ctrl-Z
+    //     IEXTEN: Ctrl-V
+    raw.c_lflag &= ~(ECHO | ICANON | ISIG | IEXTEN);
+
+    // Set a timeout for read() so we can do something else while waiting
+    // Set the timeout to 1/10 of a second
+    raw.c_cc[VTIME] = 1;
+    // Set maximum amount of time read() waits before returning
+    raw.c_cc[VMIN] = 0;
 
     // Write the new terminal attributes
-    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) die("tcsetattr");
 }
 
+/*** init ***/
 
 int main() {
     enableRawMode();
 
-    char c;
-    while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q') {
+    while (1) {
+        char c = '\0';
 
+        if (read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN) die("read");
+
+        if (iscntrl(c)) {
+            printf("%d\r\n", c);
+        } else {
+            printf("%d, (%c)\r\n", c, c);
+        }
+
+        if (c == 'q') break;
     };
+
     return 0;
 }
