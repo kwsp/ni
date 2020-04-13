5d9e7cdd3ed9c69e82a9bf692743291861c5be8d
Tiger Nie <nhl0819@gmail.com>
2020-04-13 15:20:21 -0500

    fixed page_up, page_down, end nagivation

--- main.c
+++ main.c
@@ -484,15 +484,25 @@ void editorProcessKeypress() {
             E.cx = 0;
             break;
         case END_KEY:
-            E.cx = E.screencols - 1;
+            if (E.cy < E.numrows) {
+                E.cx = E.row[E.cy].size;
+            }
             break;
 
         case PAGE_UP:
         case PAGE_DOWN:
             {
+                // Move cursor to top or bottom of screen
+                if (c == PAGE_UP) {
+                    E.cy = E.rowoff;
+                } else if (c == PAGE_DOWN) {
+                    E.cy = E.rowoff + E.screenrows - 1;
+                    if (E.cy > E.numrows) E.cy = E.numrows;
+                }
+
                 int times = E.screenrows;
                 while (times--) {
-                    editorMoveCursor(c == PAGE_UP ? ARROW_UP : ARROW_DOWN);
+                    editorMoveCursor(c==PAGE_DOWN ? ARROW_DOWN : ARROW_UP);
                 }
             }
             break;
