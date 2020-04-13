599fbe6907f3d0c47f7edc6f92dd217a6d2cc046
Tiger Nie <nhl0819@gmail.com>
2020-04-13 12:28:04 -0500

    can read one line of file into buffer

--- main.c
+++ main.c
@@ -1,10 +1,16 @@
 /*** includes ***/
+
+#define _DEFAULT_SOURCE
+#define _GNU_SOURCE
+#define _BSD_SOURCE
+
 #include <ctype.h>
 #include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/ioctl.h>
+#include <sys/types.h>
 #include <termio.h>
 #include <unistd.h>
 
@@ -28,10 +34,17 @@ enum editorKeys {
 
 /*** data ***/
 
+typedef struct {
+    int size;
+    char *chars;
+} erow;
+
 struct editorConfig {
     int cx, cy;
     int screenrows;
     int screencols;
+    int numrows;
+    erow row;
     struct termios orig_termios;
 };
 
@@ -205,6 +218,34 @@ int getWindowSize(int *rows, int *cols) {
     }
 }
 
+/*** file i/o ***/
+void editorOpen(char *filename) {
+    FILE *fp = fopen(filename, "r");
+    if (!fp) die("fopen");
+
+    char *line = NULL;
+    size_t linecap = 0;
+    ssize_t linelen;
+    // getline takes a NULL lineptr, current capacity (0), and filepointer
+    // and allocates the memory for the next line it reads.
+    linelen = getline(&line, &linecap, fp);
+
+    if (linelen != -1) {
+        // Strip the newline/carriage return characters
+        while (linelen > 0 && (line[linelen-1] == '\n' || line[linelen-1] == '\r')) {
+            linelen--;
+        }
+        // Copy to our editor row buffer
+        E.row.size = linelen;
+        E.row.chars = malloc(linelen + 1);
+        memcpy(E.row.chars, line, linelen);
+        E.row.chars[linelen] = '\0';
+        E.numrows = 1;
+    }
+    free(line);
+    fclose(fp);
+}
+
 /*** dynamic string ***/
 struct abuf {
     char *b;
@@ -247,24 +288,30 @@ void editorClearScreen() {
 void editorDrawRows(struct abuf *ab) {
     int y;
     for (y=0; y < E.screenrows; y++) {
+        if (y >= E.numrows) {
+            if (E.numrows == 0 && y == E.screenrows / 3) {
+                char welcome[80];
+                int welcomelen = snprintf(welcome, sizeof(welcome),
+                        "Ni editor -- version %s", NI_VERSION);
+                if (welcomelen > E.screencols) welcomelen = E.screencols;
 
-        if (y == E.screenrows / 3) {
-            char welcome[80];
-            int welcomelen = snprintf(welcome, sizeof(welcome),
-                    "Ni editor -- version %s", NI_VERSION);
-            if (welcomelen > E.screencols) welcomelen = E.screencols;
+                int padding = (E.screencols - welcomelen) / 2;
+                if (padding) {
+                    abAppend(ab, "~", 1);
+                    padding--;
+                }
+                while (padding--) abAppend(ab, " ", 1);
 
-            int padding = (E.screencols - welcomelen) / 2;
-            if (padding) {
+                abAppend(ab, welcome, welcomelen);
+
+            } else {
                 abAppend(ab, "~", 1);
-                padding--;
             }
-            while (padding--) abAppend(ab, " ", 1);
-
-            abAppend(ab, welcome, welcomelen);
 
         } else {
-            abAppend(ab, "~", 1);
+            int len = E.row.size;
+            if (len > E.screencols) len = E.screencols;
+            abAppend(ab, E.row.chars, len);
         }
 
         // Clear line
@@ -360,13 +407,17 @@ void editorProcessKeypress() {
 void initEditor() {
     E.cx = 0;
     E.cy = 0;
+    E.numrows = 0;
 
     if (getWindowSize(&E.screenrows, &E.screencols) == -1) die("getWindowSize");
 }
 
-int main() {
+int main(int argc, char **argv) {
     enableRawMode();
     initEditor();
+    if (argc >= 2) {
+        editorOpen(argv[1]);
+    }
 
     while (1) {
         editorRefreshScreen();
